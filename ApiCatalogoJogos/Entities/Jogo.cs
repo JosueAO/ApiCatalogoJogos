d4f7db0f20d0bad3e92090ff2ca96e12170f3bea
Josué Alves de Oliveira <josueao.ms@gmail.com>
2021-07-14 16:54:13 -0300

    InputModel - Entities - Exceptions - Middleware - Repositories - Services

--- ApiCatalogoJogos/Entities/Jogo.cs
+++ ApiCatalogoJogos/Entities/Jogo.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
-namespace ExemploApiCatalogoJogos.ViewModel
+namespace ExemploApiCatalogoJogos.Entities
 {
-    public class JogoViewModel
+    public class Jogo
     {
         public Guid Id { get; set; }
         public string Nome { get; set; }
